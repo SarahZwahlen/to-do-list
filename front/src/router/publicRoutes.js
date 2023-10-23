d55d19c5eb8166c126b848d2b2413863e151f5fc
Pparg <weisspierrepaul@gmail.com>
2023-10-23 14:32:12 +0200

    Front public routes

--- front/src/router/publicRoutes.js
+++ front/src/router/publicRoutes.js
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import { Layout } from '../components/Layout/Layout'
+import { Login } from '../components/User/Login'
+import { NewUser } from '../components/User/NewUser'
+import { NotFound } from '../components/Layout/NotFound'
 
 const publicRoutes = createBrowserRouter([
   {
     path: "/",
     element: <Layout/>,
+    children: [
+      { path: 'login', component: <Login/>},
+      { path: 'new_user', component: <NewUser/>},
+      { path: "*", component: <NotFound/>}
+    ]
   }
 ])
 
