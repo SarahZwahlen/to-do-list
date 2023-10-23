b7936ea2832a3a10a176b229378411182d65ba38
Pparg <weisspierrepaul@gmail.com>
2023-10-23 14:31:14 +0200

    Front layout update

--- front/src/components/Layout/Layout.js
+++ front/src/components/Layout/Layout.js
@@ -1,8 +1,15 @@
 
+import { Outlet } from "react-router-dom"
+
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+
 export function Layout (options) {
   return(
-    <div>
-      Hello
-    </div>
+    <>
+      <Header/>
+      <Outlet />
+      <Footer/>
+    </>
   )
 }
