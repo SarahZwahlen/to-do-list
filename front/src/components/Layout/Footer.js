6be6aa4b62accb1b81607d3b886dfb89b9c2a090
Pparg <weisspierrepaul@gmail.com>
2023-10-23 16:30:37 +0200

    Front Layout

--- front/src/components/Layout/Footer.js
+++ front/src/components/Layout/Footer.js
@@ -3,12 +3,14 @@ import logo from '../../assets/images/octo-cat.png'
 export function Footer() {
   return (
     <footer>
-      <ul>
-        <li><a href="https://github.com/Pparg"><img className="git-hub-logo" src={logo}/> Pierre-Paul WEISS</a></li>
-        <li><a href="https://github.com/SarahZwahlen"><img className="git-hub-logo" src={logo}/> Sarah ZWAHLEN</a></li>
-      </ul>
-      <a href="https://github.com/SarahZwahlen">
-      </a>
+      <div className='github'>
+        <img className="git-hub-logo" src={logo}/>
+        <a href="https://github.com/Pparg" className='link'>Pierre-Paul WEISS</a>
+      </div>
+      <div className='github'>
+        <img className="git-hub-logo" src={logo}/>
+        <a href="https://github.com/SarahZwahlen" className='link'>Sarah ZWAHLEN</a>
+      </div>
     </footer>
   )
 }
--- front/src/components/Layout/Main.js
+++ front/src/components/Layout/Main.js
@@ -1,9 +1,13 @@
 import './main.scss'
-
+import { Link } from 'react-router-dom'
 export function Main () {
   return (
     <section className="main">
-      Main
+      <h1>Start your To do</h1>
+      <div className='login_container'>
+        <Link to="/login" className='link card'>Login</Link>
+        <Link to='/new_user' className='link card'>Create account</Link>
+      </div>
     </section>
   )
 }
