6803139f0ac4e468ddd46adb297d6956f35d58f8
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2023-10-24 10:17:33 +0200

    update login route

--- back/src/router.ts
+++ back/src/router.ts
@@ -2,16 +2,15 @@ import express from 'express';
 import createUserController from './user/controllers/createUser.controller';
 import logUserController from './user/controllers/logUser.controller';
 import logOutController from './user/controllers/logOut.controller';
-import createTaskController from './task/createTask.controller';
+import createTaskController from './task/controllers/createTask.controller';
 
 const router = express.Router();
 
 //GET
-
-router.get('/user/login', logUserController);
 router.get('/user/logout', logOutController);
 
 //POST
+router.post('/user/login', logUserController);
 router.post('/task/create', createTaskController);
 router.post('/user/create', createUserController);
 //PUT
