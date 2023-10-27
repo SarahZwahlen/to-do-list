ca561086bce6c1e03e45ccfd94ab9c2adebfeb3b
Pparg <weisspierrepaul@gmail.com>
2023-10-27 14:45:54 +0200

    Front bug login

--- front/src/router/privateRoutes.js
+++ front/src/router/privateRoutes.js
@@ -6,6 +6,7 @@ import { NotFound } from '../components/Layout/NotFound'
 import {UserDashboard} from '../components/User/UserDashboard'
 import { NewTodoList } from '../components/TodoList/NewTodoList'
 import { EditTodoList } from '../components/TodoList/EditTodoList'
+import { Login } from '../components/User/Login'
 
 const privateRoutes = createBrowserRouter([
   {
@@ -13,8 +14,9 @@ const privateRoutes = createBrowserRouter([
     element: <Layout/>,
     children: [
       { path: 'settings', element: <UserDashboard/> },
+      { path: 'login', element: <Login/>},
       { path: 'new_task_list', element: <NewTodoList/>},
-      {path: 'edit_todo_list/:list_id', element: <EditTodoList/>},
+      { path: 'edit_todo_list/:list_id', element: <EditTodoList/>},
       { path: '*', element: <NotFound/>}
     ]
   }
