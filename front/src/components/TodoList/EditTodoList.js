887a0f93f71a59861cef09688bd589ae837cbd56
Pparg <weisspierrepaul@gmail.com>
2023-10-27 12:31:50 +0200

    Front Update

--- front/src/components/TodoList/EditTodoList.js
+++ front/src/components/TodoList/EditTodoList.js
@@ -1,9 +1,113 @@
+import { useParams, useNavigate } from "react-router-dom"
 
+import { useEffect, useState } from "react"
+import { EditTask } from "../Task/EditTask"
 
 export function EditTodoList () {
+  const { list_id } = useParams()
+
+  console.log(list_id)
+  const [list_data, setListData] = useState({title: ''})
+  const [tasks, setTasks] = useState([])
+
+  const [newTodo, setNewTodo] = useState({
+    title: ''
+  })
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    fetch(`http://localhost:3000/task-list/all-tasks-of-list/${list_id}`, {
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(data => {
+        setListData({title: data.data.title})
+        setTasks(data.data.tasks)
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  const handleDeleteList = () => {
+    fetch('http://localhost:3000/task-list', {
+      method: 'DELETE',
+      credentials: 'include',
+      body: JSON.stringify({taskListId: list_id}),
+      headers: { 'content-type': 'application/json'}
+    })
+      .then(res => res.json())
+      .then(data => navigate('/'))
+      .catch(err => console.log(err))
+  }
+
+  const handleNewTodo = (e) => {
+    if ((e.key === "Enter" || e.type === "click") && newTodo.title.length > 0) {
+      fetch("http://localhost:3000/task-list/add-task", {
+        method: "PUT",
+        body: JSON.stringify({
+          taskListId: list_id,
+          ...newTodo,
+        }),
+        credentials: "include",
+        headers: { "Content-type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setTasks([data.data.task, ...tasks]);
+          setNewTodo({ title: "" });
+        });
+    }
+  };
+
+  const handleDelete = (id) => {
+    fetch("http://localhost:3000/task/delete", {
+      method: "DELETE",
+      body: JSON.stringify({ taskId: id }),
+      credentials: "include",
+      headers: { "content-type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message === "Task has been deleted") {
+          setTasks(tasks.filter((todo) => todo.id !== id));
+        }
+      })
+      .catch((error) => console.log(error));
+  };
+
   return(
-    <div>
-      EditTodoList
-    </div>
+    <section className="new_list_container">
+      <div className="card">
+        <div className="list_todo_container">
+          <div className="list_todo_header">
+            <h1>{ list_data.title}</h1>
+            <p className='p-error' onClick={handleDeleteList}><i className="fa-solid fa-trash"></i></p>
+          </div>
+        </div>
+        <aside className="new_todo">
+          <div className="card todo_card">
+            <div className="todo_input_container">
+              <div className="checkbox_container">
+                <div className="checkbox"></div>
+              </div>
+              <input type="text" placeholder="Create a new todo.." value={newTodo.title} onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })} onKeyDown={handleNewTodo}></input>
+            </div>
+            <div className="input_icon">
+              <i className="fa-solid fa-plus" onClick={handleNewTodo}></i>
+            </div>
+          </div>
+          <div className="todo_list_container">
+            {  tasks.map(item => (
+              <div className="card todo_card" key={item._id}>
+                <EditTask model={item}/>
+                <div className="input_icon">
+                  <i className="fa-solid fa-xmark" onClick={() => handleDelete(item._id)}></i>
+                </div>
+              </div>
+            ))}
+          </div>
+          <p>{tasks.length} item</p>
+        </aside>
+      </div>
+    </section>
   )
 }
--- front/src/router/privateRoutes.js
+++ front/src/router/privateRoutes.js
@@ -5,6 +5,7 @@ import { Layout } from '../components/Layout/Layout'
 import { NotFound } from '../components/Layout/NotFound'
 import {UserDashboard} from '../components/User/UserDashboard'
 import { NewTodoList } from '../components/TodoList/NewTodoList'
+import { EditTodoList } from '../components/TodoList/EditTodoList'
 
 const privateRoutes = createBrowserRouter([
   {
@@ -13,6 +14,7 @@ const privateRoutes = createBrowserRouter([
     children: [
       { path: 'settings', element: <UserDashboard/> },
       { path: 'new_task_list', element: <NewTodoList/>},
+      {path: 'edit_todo_list/:list_id', element: <EditTodoList/>},
       { path: '*', element: <NotFound/>}
     ]
   }
